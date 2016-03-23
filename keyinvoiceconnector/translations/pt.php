aeede47b967c847a6ce3cced61af5470295630c0
Miguelix <nb_miguel@hotmail.com>
2016-03-23 22:58:35 +0000

    Additional translations

--- keyinvoiceconnector/translations/pt.php
+++ keyinvoiceconnector/translations/pt.php
@@ -2,15 +2,15 @@
 
 global $_MODULE;
 $_MODULE = array();
-$_MODULE['<{keyinvoiceconnector}prestashop>keyinvoiceconnector_8f838cf5ef326c8e43f420b60d1d8ee3'] = 'KeyInvoice Connector';
+$_MODULE['<{keyinvoiceconnector}prestashop>keyinvoiceconnector_ba9b0ec6acf6aa0176a1c94e14c71599'] = ' KeyInvoice Connector';
 $_MODULE['<{keyinvoiceconnector}prestashop>keyinvoiceconnector_57596ac48822ba9683945355b97a37cd'] = 'Integração com o sistema de faturação KeyInvoice';
 $_MODULE['<{keyinvoiceconnector}prestashop>displayadmincustomers_53474f0f5f9732759d884ff23573f40b'] = 'Cliente Guardado';
 $_MODULE['<{keyinvoiceconnector}prestashop>displayadmincustomers_6acdf7ae74e475d916ad54905ebd5350'] = 'Actualização Clientes no KeyInvoice';
 $_MODULE['<{keyinvoiceconnector}prestashop>displayadmincustomers_ecdc8612489244cde4df70b6c880a8a0'] = 'Enviar para o Keyinvoice';
-$_MODULE['<{keyinvoiceconnector}prestashop>displayadminorder_8f838cf5ef326c8e43f420b60d1d8ee3'] = 'Keyinvoice Connector';
+$_MODULE['<{keyinvoiceconnector}prestashop>displayadminorder_ae08a037d2c439885e962d476a17b358'] = 'Encomenda Criada no KeyInvoice';
+$_MODULE['<{keyinvoiceconnector}prestashop>displayadminorder_ba9b0ec6acf6aa0176a1c94e14c71599'] = ' KeyInvoice Connector';
 $_MODULE['<{keyinvoiceconnector}prestashop>displayadminorder_4f67fe16b274bf31a67539fbedb8f8d3'] = 'Tipo de Documento';
 $_MODULE['<{keyinvoiceconnector}prestashop>displayadminorder_ecdc8612489244cde4df70b6c880a8a0'] = 'Enviar para o Keyinvoice';
-$_MODULE['<{keyinvoiceconnector}prestashop>displayadminorder_ae08a037d2c439885e962d476a17b358'] = 'Encomenda Criada no KeyInvoice';
 $_MODULE['<{keyinvoiceconnector}prestashop>displayadminproductsextra_a3ed1ac957c5b3334cecd87c0aaafd63'] = 'Não foi possível contactar o KeyInvoice, tente mais tarde. ';
 $_MODULE['<{keyinvoiceconnector}prestashop>displayadminproductsextra_016e3960e1c210d1522c879806742d0d'] = 'Integração de produtos no KeyInvoice';
 $_MODULE['<{keyinvoiceconnector}prestashop>displayadminproductsextra_56998f876ce39162431cd04746378c41'] = 'Criar / Atualizar produto no KeyInvoice';
@@ -29,8 +29,9 @@ $_MODULE['<{keyinvoiceconnector}prestashop>getcontent_670dc901c0300b41ab20cb1691
 $_MODULE['<{keyinvoiceconnector}prestashop>getcontent_e6d0e1c8fc6a4fcf47869df87e04cd88'] = 'Clientes';
 $_MODULE['<{keyinvoiceconnector}prestashop>getcontent_a748db05999d0061548a3b072a717f29'] = 'Ativar integração com Keyinvoice e sempre que grava informação de um cliente ela é atualizada no Keyinvoice.';
 $_MODULE['<{keyinvoiceconnector}prestashop>getcontent_7442e29d7d53e549b78d93c46b8cdcfc'] = 'Encomendas';
-$_MODULE['<{keyinvoiceconnector}prestashop>getcontent_1ec79ed355bce1451a3336191c7fe6c2'] = 'Documento Base';
+$_MODULE['<{keyinvoiceconnector}prestashop>getcontent_5a11a5892fd213c35dfcbb2ef2dce3cf'] = 'Tipo de Documento Padrão';
 $_MODULE['<{keyinvoiceconnector}prestashop>getcontent_b35491a36bae1abdbcbd6a181062ba53'] = 'Ativar integração com Keyinvoice e sempre que um cliente faz uma encomenda o documento de faturação é criado no Keyinvoice.';
 $_MODULE['<{keyinvoiceconnector}prestashop>getcontent_0db377921f4ce762c62526131097968f'] = 'Configurações Gerais';
+$_MODULE['<{keyinvoiceconnector}prestashop>getcontent_9fa61be7ef525399169819ef328e90d8'] = 'Referência do Envio';
 $_MODULE['<{keyinvoiceconnector}prestashop>getcontent_399849c3ebefed13a9f883588a03d607'] = 'Para cobrar as despesas de envio deve criar um produto / serviço no Keyinvoice e guardar aqui a sua referência. Esse produto / serviço será usado para cobrar os custos de envio.';
 $_MODULE['<{keyinvoiceconnector}prestashop>getcontent_c9cc8cce247e49bae79f15173ce97354'] = 'Guardar';
