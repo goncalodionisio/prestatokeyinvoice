4d20df76ed5a70b576e655559fe4d93aeffe188c
goncalodionisio <goncalodionisio@gmail.com>
2016-06-21 01:34:11 +0100

    adicionado actEntity
    adicionado actEntity

--- prestaptinvoice/classes/OrderToPTInvoice.php
+++ prestaptinvoice/classes/OrderToPTInvoice.php
@@ -15,93 +15,7 @@
 
 class OrderToPTInvoice extends Module
 {
-    public static function sendShippingAddr(
-        $session,
-        $client,
-        $docID,
-        $getDocTypeShip,
-        $order_reference,
-        $address_delivery
-    ) {
-        $opt_deliveryLocation_address    = isset($address_delivery->address1) ? $address_delivery->address1 : 'N/A' ;
-        $opt_deliveryLocation_postalCode = isset($address_delivery->postcode) ? $address_delivery->postcode : 'N/A' ;
-        $opt_deliveryLocation_city       = isset($address_delivery->city) ? $address_delivery->city : 'N/A' ;
 
-        if ($getDocTypeShip == '15') {
-
-            $result = $client->insertDocumentHeader_additionalInfo(
-                "$session",
-                "$docID",
-                "$getDocTypeShip",
-                "",
-                "$order_reference",
-                "",
-                "",
-                "",
-                "$opt_deliveryLocation_address $opt_deliveryLocation_postalCode $opt_deliveryLocation_city",
-                "",
-                "$opt_deliveryLocation_address $opt_deliveryLocation_postalCode $opt_deliveryLocation_city",
-                "$opt_deliveryLocation_postalCode",
-                "$opt_deliveryLocation_city",
-                ""
-            );
-            return $result;
-        }
-        return array(1, "Always ok");
-
-    }
-
-    public static function sendShippingCost($session, $client, $shipping, $getDocTypeShip, $docID)
-    {
-
-        $shipping_reference = Configuration::get('PTInvoice_SHIPPINGCOST');
-
-        if ($result = $client->getProduct("$session", "$shipping_reference")) {
-
-            $Ref       = isset($result->{"DAT"}[0]->Ref) ? $result->{"DAT"}[0]->Ref : 'N/A';
-            $Name      = isset($result->{"DAT"}[0]->Name) ? $result->{"DAT"}[0]->Name : 'N/A';
-            $ShortName = isset($result->{"DAT"}[0]->ShortName) ? $result->{"DAT"}[0]->ShortName : 'N/A';
-            $TAX       = isset($result->{"DAT"}[0]->TAX) ? $result->{"DAT"}[0]->TAX : 'N/A';
-            $Obs       = isset($result->{"DAT"}[0]->Comment) ? $result->{"DAT"}[0]->Comment : 'N/A';
-            $IsService = isset($result->{"DAT"}[0]->IsService) ? $result->{"DAT"}[0]->IsService : 'N/A';
-            $HasStocks = isset($result->{"DAT"}[0]->HasStocks) ? $result->{"DAT"}[0]->HasStocks : 'N/A';
-            $Active    = isset($result->{"DAT"}[0]->Active) ? $result->{"DAT"}[0]->Active : 'N/A';
-            $ShortDescription = isset($result->{"DAT"}[0]->ShortDescription) ?
-                $result->{"DAT"}[0]->ShortDescription : 'N/A';
-            $LongDescription  = isset($result->{"DAT"}[0]->LongDescription) ?
-                $result->{"DAT"}[0]->LongDescription : 'N/A';
-            $VendorRef  = isset($result->{"DAT"}[0]->VendorRef) ? $result->{"DAT"}[0]->VendorRef : 'N/A';
-            $Price     = isset($shipping[0]['shipping_cost_tax_excl']) ?
-                $shipping[0]['shipping_cost_tax_excl'] : '0.000000';
-            $EAN     = isset($result->{"DAT"}[0]->EAN) ? $result->{"DAT"}[0]->EAN : '';
-        }
-
-        // trasportadoras - este produto n existe no presta so no PTInvoice nao se pode usar o metodo por ID
-        $result = ProductToPTInvoice::upsertProduct(
-            "$Ref",
-            "$Name",
-            "$ShortName",
-            "$TAX",
-            "$Obs",
-            "$IsService",
-            "$HasStocks",
-            "$Active",
-            "$ShortDescription",
-            "$LongDescription",
-            "$Price",
-            "$VendorRef",
-            "$EAN"
-        );
-        if (isset($result) && $result[0] != '1') {
-            return $result;
-        }
-        // custo trasportadoras
-        $result = $client->insertDocumentLine("$session", "$docID", "$getDocTypeShip", "$Ref", "1", "", "", "", "");
-        if (isset($result) && $result[0] != '1') {
-            return $result;
-        }
-        return $result;
-    }
 
     public static function sendOrderToPTInvoice($id_order, $from)
     {
@@ -158,7 +72,6 @@ class OrderToPTInvoice extends Module
             }
 
             $vat_number = isset($address_invoice->vat_number) ? $address_invoice->vat_number : '' ;
-            // $order_reference = isset($order->reference) ? $order->reference : 'N/A' ;
 
             $response = $ptinvoiceOps->login();
             if ($response[0] == "nok")
@@ -184,7 +97,6 @@ class OrderToPTInvoice extends Module
                 }
                 foreach ($cartProducts as $key => $cartProduct) {
 
-
                     // upsert product
                     ProductToPTInvoice::saveByIdProduct($ptinvoiceOps,$cartProduct['product_id']);
 
@@ -204,7 +116,6 @@ class OrderToPTInvoice extends Module
                     $newFt = $ptinvoiceOps->sendOperation("FtWS", "actEntity", $params = array('entity' => ToolsCore::jsonEncode($newFt['result'][0]), 'code' => 0, 'newValue' => ToolsCore::jsonEncode([])));
                     $newFt['result'][0]['fis'][$key]['epv'] = $product_price;
                     $newFt = $ptinvoiceOps->sendOperation("FtWS", "actEntity", $params = array('entity' => ToolsCore::jsonEncode($newFt['result'][0]), 'code' => 0, 'newValue' => ToolsCore::jsonEncode([])));
-
 
                 }
 
