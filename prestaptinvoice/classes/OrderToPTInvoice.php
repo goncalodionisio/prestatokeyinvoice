0b409f334ff56c28198e75de8f7e1f60eabe43e3
goncalodionisio <goncalodionisio@gmail.com>
2016-06-22 10:09:58 +0100

    php codesniffer
    php codesniffer

--- prestaptinvoice/classes/OrderToPTInvoice.php
+++ prestaptinvoice/classes/OrderToPTInvoice.php
@@ -8,20 +8,25 @@
  *
  * You must not modify, adapt or create derivative works of this source code
  *
- *  @author    Majoinfa - Sociedade Unipessoal Lda
+ *  @author    Majoinfa - Sociedade Unipessoal Lda <info@majoinfa.pt>
  *  @copyright 2016-2021 Majoinfa - Sociedade Unipessoal Lda
  *  @license   LICENSE.txt
  */
 
-class OrderToPTInvoice extends Module
+class OrderToPTInvoice extends ModuleCore
 {
 
 
+    /**
+     * @param $id_order
+     * @param $from
+     * @return array
+     */
     public static function sendOrderToPTInvoice($id_order, $from)
     {
-        if (Validate::isLoadedObject($order = new Order($id_order))) {
+        if (ValidateCore::isLoadedObject($order = new OrderCore($id_order))) {
 
-            $getDiscounts = $order->getDiscounts();
+            $getDiscounts = $order->getCartRules();
             if (($getDiscounts) && ($from == 'hookOrderConfirmation')) {
                 return false;
             }
@@ -31,30 +36,37 @@ class OrderToPTInvoice extends Module
 
             } else {
 
-                $getDocTypeShip = Configuration::get('PTInvoice_SHIP_DOC_TYPE');
+                $getDocTypeShip = ConfigurationCore::get('PTInvoice_SHIP_DOC_TYPE');
             }
 
             // se nao estiver configurada transportadora no presta
-            $shipping_reference = Configuration::get('PTInvoice_SHIPPINGCOST');
+            $shipping_reference = ConfigurationCore::get('PTInvoice_SHIPPINGCOST');
             if (empty($shipping_reference)) {
                 return array(-969,
                     "Aten&ccedil;&atilde;o transportadora n&atilde;o se encontra 
-                    configurada no PTInvoice Connector! Encomenda n&atilde;o sincronizada!"
+                    configurada no PTInvoice Connector! Encomenda n&atilde;o 
+                    sincronizada!"
                 );
             }
 
             $ptinvoiceOps = new PTInvoiceOperations();
             $response = $ptinvoiceOps->login();
-            if ($response[0] == "nok")
+            if ($response[0] == "nok") {
                 return $response;
-
+            }
 
             // se transportadora do presta nao for igual no PTInvoice
-            $result = $ptinvoiceOps->query("StWS", array(array('column' => 'ref', 'value' => $shipping_reference)));
+            $result = $ptinvoiceOps->query(
+                "StWS",
+                array(array(
+                    'column' => 'ref',
+                    'value' => $shipping_reference))
+            );
             if (count($result['result']) == 0) {
                 return array(-969,
-                    "Aten&ccedil;&atilde;o transportadora \"$shipping_reference\" n&atilde;o se encontra 
-                    configurada no PHCX! Encomenda n&atilde;o sincronizada!"
+                    "Aten&ccedil;&atilde;o transportadora \"$shipping_reference\" 
+                    n&atilde;o se encontra configurada no PHCX! Encomenda 
+                    n&atilde;o sincronizada!"
                 );
             }
 
@@ -63,26 +75,40 @@ class OrderToPTInvoice extends Module
             //$address_delivery = new AddressCore($order->id_address_delivery);
 
             // upsert customer
-            $response = ClientToPTInvoice::saveByIdAddress($order->id_address_invoice);
+            $response = ClientToPTInvoice::saveByIdAddress(
+                $order->id_address_invoice
+            );
             $status = PTInvoiceOperations::ResponseStatus($response);
             if ($status[0] == 'nok') {
                 return null;
             }
 
-            $vat_number = isset($address_invoice->vat_number) ? $address_invoice->vat_number : '' ;
+            $vat_number = isset($address_invoice->vat_number)
+                ? $address_invoice->vat_number : '' ;
 
             $response = $ptinvoiceOps->login();
-            if ($response[0] == "nok")
+            if ($response[0] == "nok") {
                 return $response;
+            }
 
             // client exists
-            $client = $ptinvoiceOps->query("ClWS", array(array('column' => 'ncont', 'value' => $vat_number)));
+            $client = $ptinvoiceOps->query(
+                "ClWS",
+                array(array('column' => 'ncont', 'value' => $vat_number))
+            );
             $status = PTInvoiceOperations::ResponseStatus($client);
-            if ($status[0] == 'nok') { return $status; }
+            if ($status[0] == 'nok') {
+                return $status;
+            }
 
-            $newFt = $ptinvoiceOps->newInstance("FtWS",$params = array( 'ndos' => $getDocTypeShip));
+            $newFt = $ptinvoiceOps->newInstance(
+                "FtWS", $params = array( 'ndos' => $getDocTypeShip)
+            );
+
             $status = PTInvoiceOperations::ResponseStatus($newFt);
-            if ($status[0] == 'nok') { return $status; }
+            if ($status[0] == 'nok') {
+                return $status;
+            }
 
             // get stamp
             $IdFtStamp = $newFt['result'][0]['ftstamp'];
@@ -96,24 +122,51 @@ class OrderToPTInvoice extends Module
                 foreach ($cartProducts as $key => $cartProduct) {
 
                     // upsert product
-                    ProductToPTInvoice::saveByIdProduct($ptinvoiceOps,$cartProduct['product_id']);
+                    ProductToPTInvoice::saveByIdProduct(
+                        $ptinvoiceOps, $cartProduct['product_id']
+                    );
 
                     $product_reference = isset($cartProduct['product_reference']) ?
                         $cartProduct['product_reference'] : 'N/A';
 
-                    $newFt = $ptinvoiceOps->sendOperation("FtWS", "addNewFIsByRef", $params = array('IdFtStamp' => $IdFtStamp, 'refsIds' => '["' . $product_reference . '"]', 'fiStampEditing' => ""));
+                    $newFt = $ptinvoiceOps->sendOperation(
+                        "FtWS",
+                        "addNewFIsByRef",
+                        $params = array(
+                            'IdFtStamp' => $IdFtStamp,
+                            'refsIds' => '["' . $product_reference . '"]',
+                            'fiStampEditing' => "")
+                    );
+
                     $status = PTInvoiceOperations::ResponseStatus($newFt);
-                    if ($status[0] == 'nok') { return $status; }
+                    if ($status[0] == 'nok') {
+                        return $status;
+                    }
 
                     $product_quantity = isset($cartProduct['product_quantity']) ?
                         $cartProduct['product_quantity'] : '0';
-                    $product_price = isset($cartProduct['product_price']) ? $cartProduct['product_price'] : '0';
+                    $product_price = isset($cartProduct['product_price']) ?
+                        $cartProduct['product_price'] : '0';
 
                     //Quantity and Price of FT
                     $newFt['result'][0]['fis'][$key]['qtt'] = $product_quantity;
-                    $newFt = $ptinvoiceOps->sendOperation("FtWS", "actEntity", $params = array('entity' => Tools::jsonEncode($newFt['result'][0]), 'code' => 0, 'newValue' => Tools::jsonEncode([])));
+                    $newFt = $ptinvoiceOps->sendOperation(
+                        "FtWS",
+                        "actEntity",
+                        $params = array(
+                            'entity' => Tools::jsonEncode($newFt['result'][0]),
+                            'code' => 0,
+                            'newValue' => Tools::jsonEncode([]))
+                    );
                     $newFt['result'][0]['fis'][$key]['epv'] = $product_price;
-                    $newFt = $ptinvoiceOps->sendOperation("FtWS", "actEntity", $params = array('entity' => Tools::jsonEncode($newFt['result'][0]), 'code' => 0, 'newValue' => Tools::jsonEncode([])));
+                    $newFt = $ptinvoiceOps->sendOperation(
+                        "FtWS",
+                        "actEntity",
+                        $params = array(
+                            'entity' => Tools::jsonEncode($newFt['result'][0]),
+                            'code' => 0,
+                            'newValue' => Tools::jsonEncode([]))
+                    );
 
                 }
 
@@ -122,17 +175,47 @@ class OrderToPTInvoice extends Module
 
                 if ($shipping[0]['shipping_cost_tax_excl'] != "0.000000") {
 
-                    $newFt = $ptinvoiceOps->sendOperation("FtWS", "addNewFIsByRef", $params = array('IdFtStamp' => $IdFtStamp, 'refsIds' => '["' . $shipping_reference . '"]', 'fiStampEditing' => ""));
+                    $newFt = $ptinvoiceOps->sendOperation(
+                        "FtWS",
+                        "addNewFIsByRef",
+                        $params = array(
+                            'IdFtStamp' => $IdFtStamp,
+                            'refsIds' => '["' . $shipping_reference . '"]',
+                            'fiStampEditing' => "")
+                    );
+
                     $status = PTInvoiceOperations::ResponseStatus($newFt);
-                    if ($status[0] == 'nok') { return $status; }
+                    if ($status[0] == 'nok') {
+                        return $status;
+                    }
 
                     //Quantity and Price of FT
-                    $newFt['result'][0]['fis'][COUNT($cartProducts)]['qtt'] = 1;
-                    $newFt = $ptinvoiceOps->sendOperation("FtWS", "actEntity", $params = array('entity' => Tools::jsonEncode($newFt['result'][0]), 'code' => 0, 'newValue' => Tools::jsonEncode([])));
-                    $newFt['result'][0]['fis'][COUNT($cartProducts)]['epv'] = $shipping[0]['shipping_cost_tax_excl'];
-                    $newFt = $ptinvoiceOps->sendOperation("FtWS", "actEntity", $params = array('entity' => Tools::jsonEncode($newFt['result'][0]), 'code' => 0, 'newValue' => Tools::jsonEncode([])));
+                    $newFt['result'][0]['fis'][count($cartProducts)]['qtt'] = 1;
+                    $newFt = $ptinvoiceOps->sendOperation(
+                        "FtWS",
+                        "actEntity",
+                        $params = array(
+                            'entity' => Tools::jsonEncode($newFt['result'][0]),
+                            'code' => 0,
+                            'newValue' => Tools::jsonEncode([]))
+                    );
+
+                    $newFt['result'][0]['fis'][count($cartProducts)]['epv']
+                        = $shipping[0]['shipping_cost_tax_excl'];
+
+                    $newFt = $ptinvoiceOps->sendOperation(
+                        "FtWS",
+                        "actEntity",
+                        $params = array(
+                            'entity' => Tools::jsonEncode($newFt['result'][0]),
+                            'code' => 0,
+                            'newValue' => Tools::jsonEncode([]))
+                    );
+
                     $status = PTInvoiceOperations::ResponseStatus($newFt);
-                    if ($status[0] == 'nok') { return $status; }
+                    if ($status[0] == 'nok') {
+                        return $status;
+                    }
                 }
 
                 //Associate client to FT
@@ -142,16 +225,27 @@ class OrderToPTInvoice extends Module
                 $newFt['result'][0]['efinv'] = 0;
                 $newFt['result'][0]['fin'] = 0;
 
-                $newFt = $ptinvoiceOps->sendOperation("FtWS", "actEntity", $params = array('entity' => Tools::jsonEncode($newFt['result'][0]), 'code' => 0, 'newValue' => Tools::jsonEncode([])));
+                $newFt = $ptinvoiceOps->sendOperation(
+                    "FtWS",
+                    "actEntity",
+                    $params = array(
+                        'entity' => Tools::jsonEncode($newFt['result'][0]),
+                        'code' => 0,
+                        'newValue' => Tools::jsonEncode([]))
+                );
                 $status = PTInvoiceOperations::ResponseStatus($newFt);
-                if ($status[0] == 'nok') { return $status; }
+                if ($status[0] == 'nok') {
+                    return $status;
+                }
 
                 $result = $ptinvoiceOps->save("FtWS", $newFt['result'][0]);
                 $status = PTInvoiceOperations::ResponseStatus($result);
-                if ($status[0] == 'nok') { return $status; }
+                if ($status[0] == 'nok') {
+                    return $status;
+                }
                 $ptinvoiceOps->logout();
 
-                $getDiscounts = $order->getDiscounts();
+                $getDiscounts = $order->getCartRules();
                 if ($getDiscounts) {
                     return array(-969,
                         "Aten&ccedil;&atilde;o h&aacute; descontos por sicronizar
